5e42d5327bb974c30686cea82ff71c88d25aeba8
shou <s.s19870831@gmail.com>
2024-10-30 23:22:07 +0900

    サイト全体にrevalifateを設定する

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,8 +13,6 @@ type Props = {
   };
 };
 
-export const revalidate = 60;
-
 export default async function Page({ params, searchParams }: Props) {
   console.log("Slug:", params.slug);
   console.log("Draft Key:", searchParams.dk);
